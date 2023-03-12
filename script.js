d6210dbbcc0cffa4582e70cd41bc88fbfe72e440
Vurakara Shathrughna Rahul <66429511+Rahulvs2000@users.noreply.github.com>
2023-03-12 22:32:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function fun(){
+let a;
+a = document.getElementById("Name").value;
+let p = document.getElementById("dis");
+switch(a){
+  case "0":
+    alert("name select chy ra poooo");
+     break;
+  case "1":
+    alert("hi Datta");
+    alert("nuv yavariki thakuvaa kadhuu ra andharikiii nuvee role model");
+     break;
+  case "2":
+    alert("hi Nikhil");
+    alert("CEO of future Company");
+    alert("company like Amazon, Google, Apple, NikilLvG, DN ");
+     break;
+  case "3":
+    alert(" hi Vathsav");
+    alert("epudud kadhuu lee tharuvta chapthaa");
+    alert("apuduu varaku nen dhachii pedthaa");
+     break;
+  case "4":
+    alert("hi Rahul");
+    alert("po po ni gurchii em chapalenuu po po");
+     break;
+  case "5":
+    alert(" hi Rana");
+    alert("soil pinaaa kalu petaleduuu  kani course based on soil");
+    alert("best of luck");
+     break;
+}
+}
+
+
+function img(){
+  alert("img ni kadhuu ra babu pinaa undhiii akadaa select chy");
+}
